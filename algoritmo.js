4abd7512c1644a46ba25567681b53dea99b1882c
rangelvasquezp <128017135+rangelvasquezp@users.noreply.github.com>
2023-04-09 16:37:26 -0300

    practica1-4 terminada

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+<details>
+        <summary>titulo de informacion importante</summary>
+        <img height="500" width="800" src="https://www.freejpg.com.ar/asset/900/a3/a385/F100004703.jpg" alt="imagen de pisaje">
+
+        <iframe width="450" height="450"  src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+        <p>Conseguí los mejores beneficios en Mercado Libre</p>
+        <a href="https://www.mercadolibre.com.ar/">mejores precios/</a>
+    </details>
+` ;
